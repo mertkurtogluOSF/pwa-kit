04153168cd34102595fd68630daf6d2f585e23b7
mertkurtogluOSF <mert.kurtoglu@osf.digital>
2022-10-13 13:16:36 +0300

    Day 1 - Exercise 4

--- pwa-training-mert/app/theme/components/project/header.js
+++ pwa-training-mert/app/theme/components/project/header.js
@@ -11,7 +11,7 @@ export default {
             minWidth: 'xs',
             width: 'full',
             boxShadow: 'base',
-            backgroundColor: 'white'
+            backgroundColor: 'red.500'
         },
         content: {
             maxWidth: 'container.xxxl',
